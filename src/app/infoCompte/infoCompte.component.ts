ae82a7b7fed5c972fcdcb4a3d554e31502065e40
Vincent G-C <vincent.jacques@hotmail.fr>
2020-06-12 16:30:35 +0200

    merge effectif

--- src/app/infoCompte/infoCompte.component.ts
+++ src/app/infoCompte/infoCompte.component.ts
@@ -1,19 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
-=======
+
 import { FormControl } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Event } from '../objets/event';
 import { EventService } from '../services/event.service';
->>>>>>> 7f1e97250b934c5214a475a22f9cea43878c8b3b
 import { Utilisateur } from '../objets/utilisateur';
 import { AuthenticationService } from '../services/authentication.service';
 import { UtilisateurService } from '../services/utilisateur.service';
 import { first } from 'rxjs/operators';
-import { Router } from '@angular/router';
 
-<<<<<<< HEAD
-@Component({ templateUrl: 'infoCompte.component.html' })
+@Component({
+  selector: 'app-info-compte',
+  templateUrl: './infoCompte.component.html',
+  styleUrls: ['./infoCompte.component.scss']
+})
 
 export class InfoCompteComponent implements OnInit {
   currentUser: Utilisateur;
@@ -32,13 +32,7 @@ export class InfoCompteComponent implements OnInit {
   if(this.currentUser.role==="ROLE_MODERATEUR")
     this.loadAllUsers();
   }
-=======
-@Component({
-  selector: 'app-info-compte',
-  templateUrl: './infoCompte.component.html',
-  styleUrls: ['./infoCompte.component.scss']
-})
-export class EventCreationComponent implements OnInit {
+
     private nom = new FormControl('');
     private lieu = new FormControl('');
     private organisateur = new FormControl('');
@@ -46,14 +40,7 @@ export class EventCreationComponent implements OnInit {
     private mode: number;
     protected event = new Event();
     protected titre = '';
-
-  constructor(private eventService: EventService, private route: ActivatedRoute, private router: Router) {
-    
-}
-
   
->>>>>>> 7f1e97250b934c5214a475a22f9cea43878c8b3b
-
   deleteUser(id: number) {
     this.utilisateurService.delete(id)
       .pipe(first())
