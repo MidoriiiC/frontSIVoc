bcdb54d18a5a212cd0fd301219b4add50603db41
MidoriiiC <49000909+MidoriiiC@users.noreply.github.com>
2020-06-20 10:44:59 +0200

    Add files via upload
    Oubli lors de la livraison

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { User } from '../objets/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  constructor(private http: HttpClient) { }
+
+  getUser(name: string, password: string): Observable<User> {
+    return this.http.get<User>('http://127.0.0.1:8080/connexion');
+  }
+  getUserById(id: number): Observable<User> { // non utilisé pour le moment
+    return this.http.get<User>('http://127.0.0.1:8080/evenements/'+id);
+  }
+  createUser(user: User): Observable<User>{
+    return this.http.post<User>('http://127.0.0.1:8080/connexion/creation', user);
+  }
+  modifyUser(user: User): Observable<User>{
+    return this.http.put<User>('http://127.0.0.1:8080/users/'+user.id, user);
+  }
+  getAll(){
+    return this.http.get<Array<User>>('http://127.0.0.1:8080/users');
+  }
+  delete(id: number){
+    //return this.http.delete<User>('http://127.0.0.1:8080/User/'+User.id
+    return this.http.delete('aaaa');
+  }
+  getInfoUser ():Observable<any> {
+    return this.http.get('http://127.0.0.1:8080/connexion/infos');
+  }
+  createUserRole(user: User): Observable<User>{
+    return this.http.post<User>('http://127.0.0.1:8080/users/createUserWithAuthority', user);
+  }
+}
