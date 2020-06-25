68e7c0dd8106ca9f6ba3cfc29758c2c849e460a4
Vincent G-C <vincent.jacques@hotmail.fr>
2020-06-25 10:30:57 +0200

    oublis

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -6,53 +6,50 @@ import { UserService } from '../services/user.service';
 import { AuthenticationService } from '../services/authentication.service';
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+	selector: 'app-register',
+	templateUrl: './register.component.html',
+	styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
-  registerForm: FormGroup;
-  loading = false;
-  submitted = false;
-  returnUrl: string;
-  titre='Inscription';
-    protected user = new User();
+	registerForm: FormGroup;
+	loading = false;
+	submitted = false;
+	returnUrl: string;
+	titre = 'Inscription';
+	protected user = new User();
 
-    constructor(
-      private userService: UserService,
-      private route: ActivatedRoute,
-      private formBuilder: FormBuilder,
-      private authenticationService: AuthenticationService,
-      private router: Router
-      ) {
-        // redirect to home if already logged in
-        if (this.authenticationService.currentUserValue) {
-          this.router.navigate(['/']);
-      }
-     }
-     ngOnInit() {
-      this.registerForm = this.formBuilder.group({
-        name: ['', Validators.required],
-        firstname: ['', Validators.required],
-        email: ['', Validators.required],
-        password: ['', Validators.required]
-    });
-    }
+	constructor(
+		private userService: UserService,
+		private route: ActivatedRoute,
+		private formBuilder: FormBuilder,
+		private authenticationService: AuthenticationService,
+		private router: Router
+	) {
+		// redirect to home if already logged in
+		if (this.authenticationService.currentUserValue) {
+			this.router.navigate(['/']);
+		}
+	}
+	ngOnInit() {
+		this.registerForm = this.formBuilder.group({
+			name: ['', Validators.required],
+			firstname: ['', Validators.required],
+			email: ['', Validators.required],
+			password: ['', Validators.required]
+		});
+	}
 
-    get f() { return this.registerForm.controls; }
+	get f() { return this.registerForm.controls; }
 
-    createAccount(){
-      let userCreation = new User();
-      userCreation.name = this.f.name.value;
-      userCreation.firstname = this.f.firstname.value;
-      userCreation.email = this.f.email.value;
-      userCreation.password = this.f.password.value;
-        this.userService.createUser(userCreation)
-        .subscribe(data => {
-          this.router.navigate(['/'])
-        }, err => {
-          // TODO une fois que les erreurs seront gérées dans le back (prochaine phase)
-          console.log("erreur d'enregistrement");
-        });
-    }
+	createAccount() {
+		let userCreation = new User();
+		userCreation.name = this.f.name.value;
+		userCreation.firstname = this.f.firstname.value;
+		userCreation.email = this.f.email.value;
+		userCreation.password = this.f.password.value;
+		this.userService.createUser(userCreation)
+			.subscribe(() => {
+				this.router.navigate(['/'])
+			});
+	}
 }
